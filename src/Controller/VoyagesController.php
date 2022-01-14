2be834c8d8fb595b3cacc4171cdd8f263b93f222
kevinbongo <bongokevin@live.fr>
2022-01-14 09:33:36 +0100

    Phase 3: Introduction à Twig et Simplication du Controller

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -2,39 +2,20 @@
 
 
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 
 /**
- * Description of AccueilController
+ * Description of VoyagesController
  *
  * @author kevin
  */
-
-
-class AccueilController extends AbstractController {
-    
-    /**
-     * 
-     * @var Environment
-     */
-    
-  //  private $twig;
-    /**
-     * 
-     * @param Environment $twig
-     */
- //   public function __construct(Environment $twig) {
- //       $this->twig = $twig;
- //   }
-
+class VoyagesController extends AbstractController {
     /**
      * @route("/",name="accueil")
      * @return Response
      */
     public function index():Response{
         return $this->render("pages/acceuil.html.twig");
-    }
 }
